9396c96f5a7a255a0dcbd24423ff49433ccf61b0
Gong Xian <theo@minuspoint.com>
2021-04-14 15:48:46 -0400

    Mr. OTCS can have a little refactoring, as a treat Write schedule and play video in separate processes (fix #1) Move `get_length()` to secondary function of `write_schedule()`

--- otcs.py
+++ otcs.py
@@ -4,6 +4,7 @@ import itertools
 import os
 import subprocess
 import sys
+from multiprocessing import Process
 
 ###############################################################################
 # Configuration.
@@ -45,25 +46,25 @@ SCHEDULE_UPCOMING_LENGTH = 10
 ###############################################################################
 # Function definitions.
 
-def get_length(file):
-    """Run ffprobe and retrieve length of file."""
-
-    result = subprocess.run([FFPROBE_PATH,"-v","error","-select_streams","v:0",
-                            "-show_entries","stream=duration","-of",
-                            "default=noprint_wrappers=1:nokey=1",file],
-                            capture_output=True,text=True).stdout
-    if result == "":
-        raise Exception("ffprobe was unable to read duration of: " + file)
-        
-    return result
-    
-
 def write_schedule(file_list,previous_file = ""):
     """
     Write an HTML file containing file names and lengths read from a list
     containing video file paths. Optionally, include the most recently played
     file as well.
     """
+
+    def get_length(file):
+        """Run ffprobe and retrieve length of file."""
+
+        result = subprocess.run([FFPROBE_PATH,"-v","error","-select_streams","v:0",
+                                "-show_entries","stream=duration","-of",
+                                "default=noprint_wrappers=1:nokey=1",file],
+                                capture_output=True,text=True).stdout
+        
+        if result == "":
+            raise Exception("ffprobe was unable to read duration of: " + file)
+            
+        return result
 
     # next_time contains start times of upcoming videos.
     # For the first file in file_list, this is the current system time.
@@ -123,70 +124,95 @@ except FileNotFoundError:
 # If MEDIA_PLAYLIST is a file, open the file.
 if isinstance(MEDIA_PLAYLIST,str):
     with open(MEDIA_PLAYLIST,"r") as media_playlist_file:
-        MEDIA_PLAYLIST = media_playlist_file.read().splitlines() 
+        media_playlist = media_playlist_file.read().splitlines() 
 
-# Remove blank lines and comment entries in MEDIA_PLAYLIST.
-MEDIA_PLAYLIST = [i for i in MEDIA_PLAYLIST if i != "" and not i.startswith(";")
-                  and not i.startswith("#") and not i.startswith("//")]
+elif isinstance(MEDIA_PLAYLIST,list):
+    media_playlist = MEDIA_PLAYLIST
+    
+else:
+    raise Exception("MEDIA_PLAYLIST is not a file or Python list.")
 
-# Loop over playlist indefinitely.
-while True:
-    if play_index < len(MEDIA_PLAYLIST):
+# Remove blank lines and comment entries in media_playlist.
+media_playlist = [i for i in media_playlist if i != "" 
+                  and not i.startswith(";")
+                  and not i.startswith("#") 
+                  and not i.startswith("//")]
 
-        video_time = datetime.datetime.now()
-        video_file = MEDIA_PLAYLIST[play_index]
-        video_file_fullpath = os.path.join(BASE_PATH,video_file)
+if __name__ == "__main__":
 
-        # Check if video_file exists and raise exception if it does not.
-        if not os.path.isfile(video_file_fullpath):
-            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), 
-                                    video_file_fullpath)
+    # Loop over playlist indefinitely.
+    while True:
+        if play_index < len(media_playlist):
 
-        # Write history of played video files and timestamps, limited to 
-        # PLAY_HISTORY_LENGTH.
-        if PLAY_HISTORY_LENGTH > 0:
-            with open(os.path.join(BASE_PATH,"play_history.txt"),"r") as play_history:
-                play_history_buffer = play_history.readlines()
+            video_time = datetime.datetime.now()
+            video_file = media_playlist[play_index]
+            video_file_fullpath = os.path.join(BASE_PATH,video_file)
 
-            with open(os.path.join(BASE_PATH,"play_history.txt"),"w+") as play_history:
-                play_history_buffer.append("{},{}\n".format(video_time,video_file))
-                play_history.writelines(play_history_buffer[-PLAY_HISTORY_LENGTH:])
+            # Check if video_file exists and raise exception if it does not.
+            if not os.path.isfile(video_file_fullpath):
+                raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), 
+                                        video_file_fullpath)
 
-        # TODO: Write schedule in second thread.
-        # If HTML schedule writing is enabled, retrieve next videos in list up
-        # to SCHEDULE_UPCOMING_LENGTH and pass to write_schedule.
-        if SCHEDULE_PATH != None:
-            # Copy of media list sliced from current video to the end.
-            media_progress = MEDIA_PLAYLIST[play_index:]
+            # Write history of played video files and timestamps, limited to 
+            # PLAY_HISTORY_LENGTH.
+            if PLAY_HISTORY_LENGTH > 0:
+                with open(os.path.join(BASE_PATH,"play_history.txt"),"r") as play_history:
+                    play_history_buffer = play_history.readlines()
 
-            # Pass sliced list to write_schedule.
-            if len(media_progress) >= SCHEDULE_UPCOMING_LENGTH:
-                media_copy = media_progress[:SCHEDULE_UPCOMING_LENGTH + 1]
+                with open(os.path.join(BASE_PATH,"play_history.txt"),"w+") as play_history:
+                    play_history_buffer.append("{},{}\n".format(video_time,video_file))
+                    play_history.writelines(play_history_buffer[-PLAY_HISTORY_LENGTH:])
+
+            print("Now playing: " + video_file)
+
+            # TODO: Delay playback for several seconds to account for window 
+            # capture delay.
+
+            # If HTML schedule writing is enabled, retrieve next videos in 
+            # list up to SCHEDULE_UPCOMING_LENGTH and write_schedule in second
+            # process.
+            if SCHEDULE_PATH != None:
+                # Copy of media list sliced from current video to the end.
+                media_progress = media_playlist[play_index:]
+
+                # Pass sliced list to write_schedule.
+                if len(media_progress) >= SCHEDULE_UPCOMING_LENGTH:
+                    media_copy = media_progress[:SCHEDULE_UPCOMING_LENGTH + 1]
+                
+                # If media_progress is shorter than SCHEDULE_UPCOMING_LENGTH, 
+                # copyfull media playlist until the correct length is reached.
+                else:
+                    media_copy = (media_progress 
+                                 + list(itertools.islice(
+                                   itertools.cycle(media_playlist),
+                                   SCHEDULE_UPCOMING_LENGTH
+                                 - len(media_progress) + 1)))
+
+                schedule_p = Process(target=write_schedule,args=(media_copy,),
+                                     kwargs={"previous_file":
+                                     media_playlist[play_index - 1]})
+                player_p = Process(target=subprocess.run,args=(
+                                   [MEDIA_PLAYER_PATH,video_file_fullpath,
+                                   MEDIA_PLAYER_ARGUMENTS],))
+
+                player_p.start()
+                schedule_p.start()
+                player_p.join()
+                schedule_p.join()
             
-            # If media_progress is shorter than SCHEDULE_UPCOMING_LENGTH, copy
-            # full media playlist until the correct length is reached.
+            # If scheduling is disabled, simply play files in single process.
             else:
-                media_copy = media_progress + list(
-                             itertools.islice(itertools.cycle(MEDIA_PLAYLIST),
-                             SCHEDULE_UPCOMING_LENGTH
-                             - len(media_progress) + 1))
+                result = subprocess.run([MEDIA_PLAYER_PATH,
+                                         video_file_fullpath,
+                                         MEDIA_PLAYER_ARGUMENTS])
 
-            write_schedule(media_copy,
-                           previous_file=MEDIA_PLAYLIST[play_index - 1])
+            # Increment play_index and write play_index.txt in BASE_PATH.
+            play_index = play_index + 1
+            with open(os.path.join(BASE_PATH,"play_index.txt"),"w") as index_file:
+                index_file.write(str(play_index))
 
-        # TODO: Delay playback for several seconds to account for window capture
-        # delay.
-        print("Now playing: " + video_file)
-        result = subprocess.run([MEDIA_PLAYER_PATH,video_file_fullpath,
-                                 MEDIA_PLAYER_ARGUMENTS])
-
-        # Increment play_index and write play_index.txt in BASE_PATH.
-        play_index = play_index + 1
-        with open(os.path.join(BASE_PATH,"play_index.txt"),"w") as index_file:
-            index_file.write(str(play_index))
-
-    else:
-        # Reset index at end of playlist. 
-        play_index = 0
-        with open(os.path.join(BASE_PATH,"play_index.txt"),"w") as index_file:
-            index_file.write("0")
+        else:
+            # Reset index at end of playlist. 
+            play_index = 0
+            with open(os.path.join(BASE_PATH,"play_index.txt"),"w") as index_file:
+                index_file.write("0")
