37993904f2183537cae2fc97788e9360c31dbba1
Gong Xian <theo@minuspoint.com>
2021-04-18 23:20:06 -0400

    Rewrite HTML schedule generation: Support removing entries starting with user-defined name patterns from appearing in schedule, adding their durations to the next entry (fix #7) Support for maximum total video length in schedule entries (fix #4) Set ffmpeg padding length as a configuration variable in conjunction with above (fix #8)

--- otcs.py
+++ otcs.py
@@ -15,12 +15,17 @@ from multiprocessing import Process
 MEDIA_PLAYER_PATH = "/usr/bin/ffmpeg"
 FFPROBE_PATH = "/usr/bin/ffprobe"
 
+# Number of seconds of black video to add between each video.
+# This is added to schedule durations for each video.
+# This can be ignored if not using ffmpeg.
+FFMPEG_PADDING = 2
+
 # Arguments to pass to media player. This should be whatever is
 # necessary to immediately exit the player after playback is completed.
 # MEDIA_PLAYER_BEFORE_ARGUMENTS are passed before the input file.
 # MEDIA_PLAYER_AFTER_ARGUMENTS are passed after the input file.
 MEDIA_PLAYER_BEFORE_ARGUMENTS = "-hide_banner -re -i"
-MEDIA_PLAYER_AFTER_ARGUMENTS = "-filter_complex \"tpad=stop_duration=2;apad=pad_dur=2\" -vcodec libx264 -b:v 1100k -acodec aac -b:a 128k -f flv -framerate 30 -g 60 rtmp://{rtmp_address}"
+MEDIA_PLAYER_AFTER_ARGUMENTS = f"-filter_complex \"tpad=stop_duration={FFMPEG_PADDING};apad=pad_dur={FFMPEG_PADDING}\" -vcodec libx264 -b:v 1100k -acodec aac -b:a 128k -f flv -framerate 30 -g 60 rtmp://{rtmp_address}"
 
 # Base path for all video files, including trailing slash.
 # This path will also contain play_index.txt and play_history.txt.
@@ -32,6 +37,18 @@ BASE_PATH = "/media/videos/"
 # Items starting with comment characters ; # or // and blank lines will
 # be skipped.
 MEDIA_PLAYLIST = "/home/pi/list.txt"
+
+# Filename pattern to exclude in schedules. This can be used to exclude
+# categories of videos such as station idents or commercials from the
+# schedule. Their durations will still be calculated and added to
+# the durations of preceding videos to keep the times aligned.
+# Set to a single string or a Python list with strings; paths
+# beginning with these strings (case insensitive) will be ignored.
+# e.g. ["Station Breaks/","Commercial"] will exclude all files in
+# the Station Breaks directory and all files and paths in BASE_PATH
+# starting with "Commercial".
+# Set to None to disable.
+SCHEDULE_EXCLUDE_FILE_PATTERN = "Station Breaks/"
 
 # Allow retrying file access if next video file cannot be opened.
 # This can be useful if BASE_PATH is a network share.
@@ -52,9 +69,12 @@ PLAY_HISTORY_LENGTH = 10
 SCHEDULE_PATH = "/var/www/schedule.html"
 
 # Number of upcoming shows to write in schedule.
-# High settings can cause delays in playing next file.
+# Set SCHEDULE_UPCOMING_LENGTH to the total number of minutes of
+# video to add to the schedule, and SCHEDULE_MAX_VIDEOS to limit
+# the number of videos.
 # Setting too high can cause MemoryError.
-SCHEDULE_UPCOMING_LENGTH = 10
+SCHEDULE_UPCOMING_LENGTH = 240
+SCHEDULE_MAX_VIDEOS = 15
 
 
 #######################################################################
@@ -93,7 +113,7 @@ def check_file(path):
         return
 
 
-def write_schedule(file_list,previous_file=""):
+def write_schedule(file_list,index,str_pattern):
     """
     Write an HTML file containing file names and lengths read from a list
     containing video file paths. Optionally, include the most recently played
@@ -113,31 +133,74 @@ def write_schedule(file_list,previous_file=""):
 
         return result
 
+    def get_next_file(list_sub,index_sub):
+        """Get next file from list, looping the list around when it
+        runs out.
+        """
+        list_iter = (i for i in list_sub[index_sub:])
+        while True:
+            try:
+                yield next(list_iter)
+            # Produce cycled list when generator runs out.
+            except StopIteration:
+                list_iter = itertools.cycle(list_sub)
+
+    # Get previous file by iterating file_list in reverse
+    # until a non-comment line that does not match
+    # SCHEDULE_EXCLUDE_FILE_PATTERN is reached.
+    prev_index = index - 1
+    while True:
+        if file_list[prev_index] is not None:
+            if not file_list[prev_index].casefold().startswith(str_pattern):
+                previous_file = file_list[prev_index]
+                break
+        else:
+            prev_index -= 1
+
     # next_time contains start times of upcoming videos.
     # For the first file in file_list, this is the current system time.
     # Time is retrieved in UTC, to be converted to user's local time
     # when they load the schedule in their browser.
     next_time = datetime.datetime.utcnow()
 
-    coming_up_next = []
-
     if previous_file != "":
         previous_file = os.path.splitext(previous_file)[0].replace("\\","/")
 
-    for filename in file_list:
-        # Get length of next video in seconds from ffprobe.
-        duration = float(get_length(os.path.join(BASE_PATH,filename)))
+    coming_up_next = []
+    duration = 0
+    total_duration = 0
 
-        # Remove .mp4 extension from file names and convert backslashes
+    # Calculate video file length and add to coming_up_next.
+    for filename in get_next_file(file_list,index):
+        if len(coming_up_next) > SCHEDULE_MAX_VIDEOS or total_duration > (SCHEDULE_UPCOMING_LENGTH * 60):
+            break
+
+        # Skip comment entries.
+        if filename is None:
+            continue
+
+        # TODO: Use check_file() for schedule generation.
+
+        # Get length of next video in seconds from ffprobe, plus
+        # ffmpeg padding.
+        duration += int(float(get_length(os.path.join(BASE_PATH,filename)))) + FFMPEG_PADDING
+        total_duration += duration
+
+        # Remove extension from filenames and convert backslashes
         # to forward slashes.
         filename = os.path.splitext(filename)[0].replace("\\","/")
 
         # Append duration and stripped filename to list as tuple.
-        coming_up_next.append((next_time,filename))
+        # Skip files matching SCHEDULE_EXCLUDE_FILE_PATTERN, but keep
+        # their durations.
+        if not filename.casefold().startswith(str_pattern):
+            coming_up_next.append((next_time,filename))
 
         # Add length of current video to current time and use as
         # starting time for next video.
-        next_time = next_time + datetime.timedelta(seconds=duration)
+        next_time += datetime.timedelta(seconds=duration)
+        duration = 0
+
 
     # Format coming_up_next list into string suitable for assigning as
     # JavaScript array of objects.
@@ -155,7 +218,7 @@ def write_schedule(file_list,previous_file=""):
     # Upload html_file to a publicly accessible location
     # using pysftp or something similar if necessary.
 
-def loop(media_playlist):
+def loop(media_playlist,str_pattern):
     """Loop over playlist indefinitely."""
 
     def play():
@@ -197,35 +260,7 @@ def loop(media_playlist):
         # second process.
         if SCHEDULE_PATH != None:
 
-            # Copy of media list sliced from current video to the end.
-            media_progress = (i for i in media_playlist[play_index:])
-            media_copy = []
-
-            # Generate list of filenames for schedule.
-            while len(media_copy) < SCHEDULE_UPCOMING_LENGTH:
-                try:
-                    next_media = next(media_progress)
-                    # Skip comment entries.
-                    if next_media != None:
-                        media_copy.append(next_media)
-
-                # Produce cycled list when generator runs out.
-                except StopIteration:
-                    media_progress = itertools.cycle(media_playlist)
-
-            # Get previous file by iterating media_playlist in reverse
-            # until a non-comment line is reached.
-            prev_index = play_index - 1
-            while True:
-                if media_playlist[prev_index] != None:
-                    previous_file = media_playlist[prev_index]
-                    break
-                else:
-                    prev_index = prev_index - 1
-
-            schedule_p = Process(target=write_schedule,args=(media_copy,),
-                                 kwargs={"previous_file":previous_file})
-
+            schedule_p = Process(target=write_schedule,args=(media_playlist,play_index,str_pattern))
             player_p = Process(target=subprocess.run,kwargs={"args":"\"{}\" {} \"{}\" {}".format(MEDIA_PLAYER_PATH,MEDIA_PLAYER_BEFORE_ARGUMENTS,video_file_fullpath,MEDIA_PLAYER_AFTER_ARGUMENTS),"shell":True})
 
             player_p.start()
@@ -239,12 +274,13 @@ def loop(media_playlist):
             result = subprocess.run("\"{}\" {} \"{}\" {}".format(MEDIA_PLAYER_PATH,MEDIA_PLAYER_BEFORE_ARGUMENTS,video_file_fullpath,MEDIA_PLAYER_AFTER_ARGUMENTS),shell=True)
 
     # Keep playlist index and store in file play_index.txt. Create it
-    # if it does not exist.
+    # if it does not exist. Reset index to 0 if it overruns the list.
     try:
         with open(os.path.join(BASE_PATH,"play_index.txt"),"r") as index_file:
             play_index = int(index_file.read())
+            media_playlist[play_index]
 
-    except FileNotFoundError:
+    except (FileNotFoundError, IndexError) as e:
         with open(os.path.join(BASE_PATH,"play_index.txt"),"w") as index_file:
             index_file.write("0")
             play_index = 0
@@ -281,8 +317,17 @@ if __name__ == "__main__":
     else:
         raise Exception("MEDIA_PLAYLIST is not a file or Python list.")
 
+    # exclude_pattern is used in case-insensitive string comparison.
+    if SCHEDULE_EXCLUDE_FILE_PATTERN is not None:
+        if isinstance(SCHEDULE_EXCLUDE_FILE_PATTERN,str):
+            exclude_pattern = (SCHEDULE_EXCLUDE_FILE_PATTERN.casefold().replace("\\","/"),)
+        elif isinstance(SCHEDULE_EXCLUDE_FILE_PATTERN,list):
+            exclude_pattern = tuple([i.casefold().replace("\\","/") for i in SCHEDULE_EXCLUDE_FILE_PATTERN])
+    else:
+        exclude_pattern = None
+
     # Change blank lines and comment entries in media_playlist to None.
     media_playlist = [i if i != "" and not i.startswith(";") and not i.startswith("#") and not i.startswith("//") else None for i in media_playlist]
 
     while True:
-        loop(media_playlist)
+        loop(media_playlist,exclude_pattern)
