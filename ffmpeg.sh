83a4f83d44aa624411bc98840d08323685f8ea3b
Gong Xian <theo@minuspoint.com>
2021-04-19 19:20:26 -0400

    Revise ffmpeg.sh script

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 FFMPEG_OUTPUT=""
+FFMPEG_PATH="/home/pi/ffmpeg-hls-pts-discontinuity-reclock/ffmpeg"
+NGINX_PATH="/usr/local/nginx/sbin/nginx"
+
+pgrep -x nginx >/dev/null
+
+if [ $? -eq 1 ]
+then
+    $NGINX_PATH
+fi
 
 while :
 do
-     ./ffmpeg-hls-pts-discontinuity-reclock/ffmpeg -i rtmp://localhost:1935/source -loglevel error -vcodec copy -acodec copy -f flv $FFMPEG_OUTPUT 2>&1 | tee -a >(ts "%b %d %Y %H:%M:%S" >> ffmpeg_error.log)
-    
+    $FFMPEG_PATH -i rtmp://localhost:1935/live -loglevel error -vcodec copy -acodec copy -f flv $FFMPEG_OUTPUT 2>&1 | tee -a >(ts "%b %d %Y %H:%M:%S" >> ffmpeg_error.log)
+
     echo -e "ffmpeg terminated. Restarting..." >&2
     sleep 1
 
 done
-    
