0914e0ed711a3513b6c369f35d7ee1c34b9ee149
Gong Xian <theo@minuspoint.com>
2024-08-20 17:10:48 -0400

    Update tests

--- tests/test_print2.py
+++ tests/test_print2.py
@@ -1,7 +1,9 @@
+import datetime
+
 def test_print2_functions(monkeypatch,capsys):
 
     monkeypatch.setattr("sys.argv", ["main.py", "./tests/test_config.ini"])
-    
+
     import config
     from config import print2
 
@@ -9,35 +11,35 @@ def test_print2_functions(monkeypatch,capsys):
 
     print2("verbose2","Super Verbose message")
     capture = capsys.readouterr()
-    assert capture.out == "[Info]" + " Super Verbose message\n"
+    assert capture.out == datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + "[Info]" + " Super Verbose message\n"
 
     print2("verbose","Verbose message")
     capture = capsys.readouterr()
-    assert capture.out == "[Info]" + " Verbose message\n"
+    assert capture.out == datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + "[Info]" + " Verbose message\n"
 
     print2("info","Info message")
     capture = capsys.readouterr()
-    assert capture.out == "[Info]" + " Info message\n"
+    assert capture.out == datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + "[Info]" + " Info message\n"
 
     print2("play","Play message")
     capture = capsys.readouterr()
-    assert capture.out == '\033[92m' + "[Play]" + '\033[0m' + " Play message\n"
+    assert capture.out == datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + '\033[92m' + "[Play]" + '\033[0m' + " Play message\n"
 
     print2("notice","Notice message")
     capture = capsys.readouterr()
-    assert capture.out == '\033[96m' + "[Notice]" + '\033[0m' + " Notice message\n"
+    assert capture.out == datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + '\033[96m' + "[Notice]" + '\033[0m' + " Notice message\n"
 
     print2("warn","Warn message")
     capture = capsys.readouterr()
-    assert capture.out == '\033[93m' + "[Warn]" + '\033[0m' + " Warn message\n"
+    assert capture.out == datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + '\033[93m' + "[Warn]" + '\033[0m' + " Warn message\n"
 
     print2("error","Error message")
     capture = capsys.readouterr()
-    assert capture.out == '\033[31m' + "[Error]" + '\033[0m' + " Error message\n"
+    assert capture.out == datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + '\033[31m' + "[Error]" + '\033[0m' + " Error message\n"
 
     print2("fatal","Fatal message")
     capture = capsys.readouterr()
-    assert capture.out == '\033[31m' + "[Error]" + '\033[0m' + " Fatal message\n"
+    assert capture.out == datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + '\033[31m' + "[Error]" + '\033[0m' + " Fatal message\n"
 
     config.VERBOSE = 0
 
