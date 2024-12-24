bdcb4b856577b600466ed30f764bdf95e6a56332
Gong Xian <theo@minuspoint.com>
2024-12-24 16:15:24 -0500

    Initialize `MAIL_ALERT_STATUS_REPORT_TIME`

--- config.py
+++ config.py
@@ -95,7 +95,7 @@ ini_defaults = {
         "MAIL_ALERT_ON_NEW_VERSION": True,
         "MAIL_ALERT_ON_NEW_PRERELEASE_VERSION": False,
         "MAIL_ALERT_STATUS_REPORT": 7,
-        "MAIL_ALERT_STATUS_REPORT_TIME_STR": "",  # The value in MAIL_ALERT_STATUS_REPORT_TIME_STR is parsed and written to MAIL_ALERT_STATUS_REPORT_TIME.
+        "MAIL_ALERT_STATUS_REPORT_TIME": "",
         "MAIL_ALERT_HIGH_PRIORITY_ERROR": True,
     },
     "Misc": {
@@ -472,7 +472,10 @@ if default_ini.has_section("Mail"):
         "Mail", "MAIL_ALERT_ON_NEW_PRERELEASE_VERSION"
     )
     MAIL_ALERT_STATUS_REPORT = default_ini.getint("Mail", "MAIL_ALERT_STATUS_REPORT")
-    MAIL_ALERT_STATUS_REPORT_TIME_STR = default_ini.get(  # Added in 2.2.1.
+    # Added in 2.2.1. 
+    MAIL_ALERT_STATUS_REPORT_TIME = (0,0)
+    # The value in MAIL_ALERT_STATUS_REPORT_TIME in config.ini is stored in this variable and parsed to MAIL_ALERT_STATUS_REPORT_TIME.
+    MAIL_ALERT_STATUS_REPORT_TIME_STR = default_ini.get(  
         "Mail", "MAIL_ALERT_STATUS_REPORT_TIME"
     )
     MAIL_ALERT_HIGH_PRIORITY_ERROR = default_ini.getboolean(
@@ -505,6 +508,7 @@ else:
     MAIL_ALERT_ON_NEW_VERSION = False
     MAIL_ALERT_ON_NEW_PRERELEASE_VERSION = False
     MAIL_ALERT_STATUS_REPORT = 0
+    MAIL_ALERT_STATUS_REPORT_TIME = (0,0)
     MAIL_ALERT_STATUS_REPORT_TIME_STR = ""
     MAIL_ALERT_HIGH_PRIORITY_ERROR = False
 
@@ -728,7 +732,9 @@ if MAIL_ENABLE:
     if MAIL_ALERT_STATUS_REPORT:
         if MAIL_ALERT_STATUS_REPORT_TIME_STR != "":
             try:
-                report_hr, report_min = map(int, MAIL_ALERT_STATUS_REPORT_TIME_STR.split(":"))
+                report_hr, report_min = map(
+                    int, MAIL_ALERT_STATUS_REPORT_TIME_STR.split(":")
+                )
                 if 0 <= report_hr <= 23 and 0 <= report_min <= 59:
                     MAIL_ALERT_STATUS_REPORT_TIME = (report_hr, report_min)
                 else:
